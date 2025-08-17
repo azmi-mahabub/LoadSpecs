7f2503afc0b7790b83bf0bd48b46fd5e561ab5d1
azmi-mahabub <mahabubuddinazmi@gmail.com>
2025-08-18 03:46:01 +0600

    new commit

--- LoadSpecsApp/views.py
+++ LoadSpecsApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def home(request):
+    return render (request, template_name='LoadSpecsHTML\home.html')
--- LoadSpecsProject/urls.py
+++ LoadSpecsProject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from LoadSpecsApp import views as s_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', s_views.home, name='home'),
 ]
